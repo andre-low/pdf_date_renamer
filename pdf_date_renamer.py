1ceec27d5542f2396ed0c8168bc049b0e9c54875
horatiojh <horatio.j.ho@gmail.com>
2023-09-27 10:30:48 +0800

    update logic for pdf renamer to include agency and name

--- pdf_date_renamer.py
+++ pdf_date_renamer.py
@@ -33,6 +33,33 @@ def find_dates(text):
                          'PARSERS': ['absolute-time']})
     return dates
 
+def find_resident_name(text):
+    # make sure to exclude matching Louis' name
+    nameRegex = "(?:MR|MS|MDM|Mr|Ms|Mdm)(?!Kheng Wee|KHENG WEE) (.*)"
+    output = re.search(nameRegex, text)
+    if not output:
+        print("No name found for this case")
+        return
+    name = output.group(1)
+    name = name.upper()
+    # replace spaces with underscores, and replace / in the name (ie. Muthu S/O Kumar) as it messes with filepath conversion
+    name = name.replace(" ", "_")
+    name = name.replace("/", "")
+    # print(name)
+    return(name)
+
+def find_agency_and_case_number(text):
+    # find our reference number, if it is provided in the return email 
+    referenceRegex = "(RV[0-9]{4})-[0-9]{6}-([^\s]+)"
+    output = re.search(referenceRegex, text)
+    if not output:
+        print("No agency found for this case, returning first line")
+        return text.split('\n', 1)[0].strip()
+    case = re.search(referenceRegex, text).group(1).strip()
+    agency = re.search(referenceRegex, text).group(2).strip()
+    # print(agency + '_' + case)
+    return(agency + '_' + case)
+
 
 def pdf_parser(path):  # Open and parses PDF, returns string with cleaned text
     output_string = StringIO()
@@ -46,16 +73,30 @@ def pdf_parser(path):  # Open and parses PDF, returns string with cleaned text
             interpreter.process_page(page)
     raw_text = (output_string.getvalue())
     cleaned_text = preclean_text(raw_text)
+    # print(cleaned_text)
     return cleaned_text
 
 
 def pdf_date_renamer(path):
     if path.endswith('.pdf'): # only acts on PDF files
         print("Trying to rename: " + path)
-        if find_dates(pdf_parser(path)): # only proceeds if a date is detected
-            detected_date = find_dates(pdf_parser(path))[0][1]
+        newFileName = ""
+        pdfText = pdf_parser(path)
+        name = find_resident_name(pdfText)
+        agencyCase = find_agency_and_case_number(pdfText)
+        # check if a date is detected
+        if find_dates(pdfText): 
+            detected_date = find_dates(pdfText)[0][1]
             formatted_date = detected_date.strftime('%Y%m%d')
-            newFileName = formatted_date + '_' + token_hex(3) + '.pdf'
+            newFileName += formatted_date + "_"
+        # check if an agency and case were generated
+        if agencyCase:
+            newFileName += agencyCase + "_"
+        # check if a name was generated
+        if name:
+            newFileName += name
+        if newFileName != "":
+            newFileName += ".pdf"
             os.rename(path, newFileName)
             print("File renamed to: " + newFileName)
         else: 
